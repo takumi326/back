55e62744ec5b6a04b5585cdd25cb45c70378f925
Hasshi <hasshi@DESKTOP-AU9HMFS>
2024-06-27 14:03:20 +0900

    cors修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8000','https://front-irjqjy911-hasshis-projects.vercel.app'
+    origins 'localhost:8000','https://tasumane.vercel.app'
     resource "*",
       headers: :any,
       expose: ["access-token", "expiry", "token-type", "uid", "client"],
