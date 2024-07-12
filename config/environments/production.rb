087073115ccf2136a565a86035b5b2b68ff42fd7
Hasshi <hasshi@DESKTOP-AU9HMFS>
2024-07-12 11:47:53 +0900

    メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,17 @@ Rails.application.configure do
   
   config.action_mailer.default_options = { from: "no-reply@example.com" }
   config.action_mailer.default_url_options = { host: 'back-red-leaf-1146.fly.dev' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.example.com",
+    port: 587,
+    domain: "example.com",
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
 
   Rails.application.configure do
     config.hosts << "back-red-leaf-1146.fly.dev"
