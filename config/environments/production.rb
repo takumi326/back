44b3b31f38a1e283b4999cb66c6ce4559e6ce8f9
Hasshi <hasshi@DESKTOP-AU9HMFS>
2024-07-14 18:30:21 +0900

    メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
     address: "tasumane7@gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: ENV["SMTP_USERNAME"],
-    password: ENV["SMTP_PASSWORD"],
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
     authentication: "plain",
     enable_starttls_auto: true
   }
